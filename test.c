5541772349843190097300ee45f1c3debb441c89
duckeatorange <144512854+duckeatorange@users.noreply.github.com>
2024-11-26 16:52:03 +0800

    Update test.c

--- test.c
+++ test.c
@@ -5,12 +5,12 @@ void vulnerable_function(char *input) {
     char buffer[10];
     // 这个代码存在缓冲区溢出漏洞
     strcpy(buffer, input);
-    print("Buffer content: %s\n", buffer);
+    printf("Buffer content: %s\n", buffer);
 }
 
 int main() {
     char user_input[50];
-    prin("Enter some text: ");
+    printf("Enter some text: ");
     gets(user_input);  // 这个代码存在 gets() 函数的漏洞
     vulnerable_function(user_input);
     return 0;
