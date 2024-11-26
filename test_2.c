99dc7b07c13fcc36169825c9df9bda8b853f67b1
duckeatorange <144512854+duckeatorange@users.noreply.github.com>
2024-11-26 15:47:16 +0800

    Create test_2.c

---
+++ test_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void bad_function() {
+    int a[5];
+    a[5] = 10;  // 错误：数组越界，访问未定义的内存
+    
+    int *ptr;
+    *ptr = 42;  // 错误：指针未初始化，解引用未初始化指针
+    
+    printf("a[5] = %d\n", a[5]);  // 错误：访问数组越界的元素
+}
+
+int main() {
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    
+    if (x == 0) {
+        printf("You entered zero\n");
+    } else {
+        printf("You entered non-zero\n");
+    }
+    
+    bad_function();  // 调用包含错误的函数
+
+    return 0;
+}
